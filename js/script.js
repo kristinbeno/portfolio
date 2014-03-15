867f056e14d807a6a2d4e1641931cd7bc1560eca
Kristin Beno <kristinbeno@gmail.com>
2014-03-15 09:38:59 -0400

    Upload Files

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+
+// Home thumbnail hover statess
+
+
+// Animate in all at once
+//	$("ul.boxes").hide();
+	
+ //   $.wait = function( callback, seconds){
+//	return window.setTimeout( callback, seconds * 1000 );
+//	}
+//	$.wait( function(){ $("ul.boxes").fadeIn() }, .5);
+
+
+
+// Animate in one at a time
+// $('ul.boxes li').hide();
+
+// $(document).ready(function(){
+// var numOfItemsToFade = 1,
+// numOfItems = $('ul.boxes li').length;
+// showList();
+// function showList() {
+// $('ul.boxes li:nth-child('+numOfItemsToFade+')').fadeIn(300, function(){
+// numOfItemsToFade++;
+// if(numOfItemsToFade<= numOfItems){
+// showList();
+// }
+// });
+// }
+// });
